fdb5022d5c779b63a51b7011dba6a6a9456026b8
azidhilman <azidhilman@gmail.com>
2025-05-31 06:32:12 +0700

    dev

--- src/features/auth/services/authApi.js
+++ src/features/auth/services/authApi.js
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const loginAPI = async (email, password) => {
   const res = await axios.post(`${API_BASE_URL}/login`, {
-    email,
+    username,
     password,
   });
   return res.data.data;
